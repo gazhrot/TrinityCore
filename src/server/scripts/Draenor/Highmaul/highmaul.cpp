566cf520bea8fb727991e50d1679dbce9482fb0b
Axel Bruneaux <gazhrot@gmail.com>
2016-01-13 19:26:36 +0100

    ajout de Highmaul.cpp

---
+++ src/server/scripts/Draenor/Highmaul/highmaul.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2016 Gazhrot
+ *
+ */
+
+#include "highmaul.h"
+#include "ScriptMgr.h"
+#include "ScriptedCreature.h"
+
+enum Spells {
+
+};
+
+enum Creatures {
+
+};
+
+enum Events {
+
+};
