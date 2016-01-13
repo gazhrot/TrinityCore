243d8a60133732d7f02faa0941bcee839f781dd4
Axel Bruneaux <gazhrot@gmail.com>
2016-01-13 19:27:01 +0100

    ajout de highmaul.h

---
+++ src/server/scripts/Draenor/Highmaul/highmaul.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2016 Gazhrot
+ *
+ */
+
+#ifndef HIGHMAUL_H_
+#define HIGHMAUL_H_
+
+#define HMScriptName "instance_highmaul"
+#define DataHeader "HM"
+
+ uint32 const EncounterCount 		= 8;
+
+ enum Data {
+
+ 	DATA_BRACKENSPORE				= 0,
+ 	DATA_KARGATHBLADEFIST			= 1,
+ 	DATA_KORAGH						= 2,
+ 	DATA_MARGOK						= 3,
+ 	DATA_TECTUS						= 4,
+ 	DATA_THEBUTCHER					= 5,
+ 	DATA_TWINOGRONPOL				= 6,
+ 	DATA_TWINOGRONPHEMOS			= 7,
+ 	DATA_VULGOR						= 8
+ };
+
+ enum CreatureIds {
+
+ 	BOSS_BRACKENSPORE				= 78491,
+ 	BOSS_KARGATHBLADEFIST			= 78714,
+ 	BOSS_KORAGH						= 79015,
+ 	BOSS_MARGOK						= 77428,
+ 	BOSS_TECTUS						= 78948,
+ 	BOSS_THEBUTCHER					= 77404,
+ 	BOSS_TWINOGRONPOL				= 78238,
+ 	BOSS_TWINOGRONPHEMOS			= 78237,
+ 	BOSS_VULGOR						= 80048
+ };
+
+ enum GameObjectIds {
+
+ };
+
+#endif
